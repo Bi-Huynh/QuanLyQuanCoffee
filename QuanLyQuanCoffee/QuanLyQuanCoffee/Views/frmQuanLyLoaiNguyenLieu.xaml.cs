54bf8f882a33665fdc6d1940010528aa3aa74b62
=Bi-Huynh <=huynhtrungduc1231998@gmail.com>
2020-06-09 23:06:28 +0700

    tao giao dien quan ly loai nguyen lieu

---
+++ QuanLyQuanCoffee/QuanLyQuanCoffee/Views/frmQuanLyLoaiNguyenLieu.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyQuanCoffee.Views
+{
+    /// <summary>
+    /// Interaction logic for frmQuanLyLoaiNguyenLieu.xaml
+    /// </summary>
+    public partial class frmQuanLyLoaiNguyenLieu : Window
+    {
+        public frmQuanLyLoaiNguyenLieu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSua_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnBoChon_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CommandBinding_Executed_ThemLoaiNguyenLieu(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void CommandBinding_CanExecute_ThemLoaiNguyenLieu(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+
+        private void btnXoa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
